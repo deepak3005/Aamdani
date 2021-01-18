a94fef75f181f2dfc1146924b8df0c55be5a8386
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-01-18 20:16:01 +0530

    Update Frame1.java

--- Frame1.java
+++ Frame1.java
@@ -8,6 +8,7 @@ import javax.swing.JTextArea;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Color;
 
 public class Frame1 {
 
@@ -41,11 +42,15 @@ public class Frame1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
 		frame.setBounds(100, 100, 680, 470);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("POST A JOB");
+		btnNewButton.setBorder(null);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(Color.DARK_GRAY);
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
 				frame.dispose();
@@ -58,13 +63,17 @@ public class Frame1 {
 		frame.getContentPane().add(btnNewButton);
 		
 		JTextArea txtrWelcome = new JTextArea();
+		txtrWelcome.setBackground(Color.LIGHT_GRAY);
 		txtrWelcome.setEditable(false);
 		txtrWelcome.setFont(new Font("Calibri", Font.BOLD, 40));
 		txtrWelcome.setText("Welcome To AAMDANI");
-		txtrWelcome.setBounds(137, 53, 406, 64);
+		txtrWelcome.setBounds(140, 56, 392, 39);
 		frame.getContentPane().add(txtrWelcome);
 		
 		JButton btnNewButton_1 = new JButton("FIND A JOB");
+		btnNewButton_1.setBorder(null);
+		btnNewButton_1.setForeground(Color.WHITE);
+		btnNewButton_1.setBackground(Color.DARK_GRAY);
 		btnNewButton_1.setFont(new Font("Calibri", Font.BOLD, 20));
 		btnNewButton_1.setBounds(376, 242, 180, 85);
 		frame.getContentPane().add(btnNewButton_1);
