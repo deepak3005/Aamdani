4bbda2214b2fe3f36c64dbc6f4ebcfd0a9162197
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-02-20 22:55:26 +0530

    Connected to database.

--- NewShopkeeperSignUp.java
+++ NewShopkeeperSignUp.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,21 +14,32 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import javax.swing.JOptionPane;
 import java.awt.SystemColor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import java.awt.Component;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.Cursor;
 
 public class NewShopkeeperSignUp extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtSdf;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_7;
+	private JTextField shopkeeper_name;
+	private JTextField shopkeeper_phone;
+	private JTextField shopkeeper_email;
+	private JTextField shopkeeper_shop_name;
+	private JTextField shopkeeper_shop_address;
+	private JPasswordField shopkeeper_password;
+	private JPasswordField shopkeeper_confirm_password;
 
 	/**
 	 * Launch the application.
@@ -56,124 +70,185 @@ public class NewShopkeeperSignUp extends JFrame {
 		contentPane.setLayout(null);
 		
 		JTextArea txtrName = new JTextArea();
+		txtrName.setBounds(53, 95, 45, 21);
 		txtrName.setEditable(false);
 		txtrName.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrName.setBackground(Color.LIGHT_GRAY);
 		txtrName.setText("Name");
-		txtrName.setBounds(53, 95, 45, 21);
 		contentPane.add(txtrName);
 		
 		JTextArea txtrPhoneNo = new JTextArea();
+		txtrPhoneNo.setBounds(53, 140, 77, 22);
 		txtrPhoneNo.setEditable(false);
 		txtrPhoneNo.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrPhoneNo.setBackground(Color.LIGHT_GRAY);
 		txtrPhoneNo.setText("Phone No.");
-		txtrPhoneNo.setBounds(53, 139, 77, 22);
 		contentPane.add(txtrPhoneNo);
 		
 		JTextArea txtrEmail = new JTextArea();
+		txtrEmail.setBounds(53, 185, 45, 21);
 		txtrEmail.setEditable(false);
 		txtrEmail.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrEmail.setBackground(Color.LIGHT_GRAY);
 		txtrEmail.setText("Email");
-		txtrEmail.setBounds(53, 182, 45, 21);
 		contentPane.add(txtrEmail);
 		
 		JTextArea txtrSetPassword = new JTextArea();
+		txtrSetPassword.setBounds(53, 230, 96, 22);
 		txtrSetPassword.setEditable(false);
 		txtrSetPassword.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrSetPassword.setBackground(Color.LIGHT_GRAY);
 		txtrSetPassword.setText("Set Password");
-		txtrSetPassword.setBounds(53, 223, 96, 22);
 		contentPane.add(txtrSetPassword);
 		
 		JTextArea txtrShopName = new JTextArea();
+		txtrShopName.setBounds(53, 320, 86, 22);
 		txtrShopName.setEditable(false);
 		txtrShopName.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrShopName.setBackground(Color.LIGHT_GRAY);
 		txtrShopName.setText("Shop name");
-		txtrShopName.setBounds(53, 267, 86, 22);
 		contentPane.add(txtrShopName);
 		
 		JTextArea txtrAddress = new JTextArea();
+		txtrAddress.setBounds(53, 365, 133, 22);
 		txtrAddress.setEditable(false);
 		txtrAddress.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrAddress.setBackground(Color.LIGHT_GRAY);
-		txtrAddress.setText("Address");
-		txtrAddress.setBounds(53, 313, 59, 22);
+		txtrAddress.setText("Shop Address");
 		contentPane.add(txtrAddress);
 		
 		JTextArea txtrDistanceFromUpes = new JTextArea();
+		txtrDistanceFromUpes.setBounds(53, 275, 133, 22);
 		txtrDistanceFromUpes.setEditable(false);
 		txtrDistanceFromUpes.setFont(new Font("Calibri", Font.BOLD, 16));
 		txtrDistanceFromUpes.setBackground(Color.LIGHT_GRAY);
-		txtrDistanceFromUpes.setText("Distance from UPES (km)");
-		txtrDistanceFromUpes.setBounds(53, 360, 179, 22);
+		txtrDistanceFromUpes.setText("Confirm Password");
 		contentPane.add(txtrDistanceFromUpes);
 		
-		txtSdf = new JTextField();
-		txtSdf.setFont(new Font("Calibri", Font.PLAIN, 15));
-		txtSdf.setBounds(241, 91, 179, 21);
-		contentPane.add(txtSdf);
-		txtSdf.setColumns(10);
+		shopkeeper_name = new JTextField();
+		shopkeeper_name.setAutoscrolls(false);
+		shopkeeper_name.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_name.setBounds(215, 90, 179, 21);
+		shopkeeper_name.setFont(new Font("Calibri", Font.PLAIN, 14));
+		contentPane.add(shopkeeper_name);
+		shopkeeper_name.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(241, 135, 179, 22);
-		contentPane.add(textField_1);
+		shopkeeper_phone = new JTextField();
+		shopkeeper_phone.setFont(new Font("Calibri", Font.PLAIN, 14));
+		shopkeeper_phone.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_phone.setBounds(215, 135, 179, 22);
+		shopkeeper_phone.setColumns(10);
+		contentPane.add(shopkeeper_phone);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(241, 178, 179, 21);
-		contentPane.add(textField_2);
+		shopkeeper_email = new JTextField();
+		shopkeeper_email.setFont(new Font("Calibri", Font.PLAIN, 14));
+		shopkeeper_email.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_email.setBounds(215, 180, 179, 21);
+		shopkeeper_email.setColumns(10);
+		contentPane.add(shopkeeper_email);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(241, 219, 179, 22);
-		contentPane.add(textField_3);
+		shopkeeper_shop_name = new JTextField();
+		shopkeeper_shop_name.setFont(new Font("Calibri", Font.PLAIN, 14));
+		shopkeeper_shop_name.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_shop_name.setBounds(215, 315, 179, 22);
+		shopkeeper_shop_name.setColumns(10);
+		contentPane.add(shopkeeper_shop_name);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(241, 263, 179, 22);
-		contentPane.add(textField_4);
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(241, 309, 179, 22);
-		contentPane.add(textField_5);
-		
-		textField_7 = new JTextField();
-		textField_7.setColumns(10);
-		textField_7.setBounds(241, 356, 179, 22);
-		contentPane.add(textField_7);
+		shopkeeper_shop_address = new JTextField();
+		shopkeeper_shop_address.setFont(new Font("Calibri", Font.PLAIN, 14));
+		shopkeeper_shop_address.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_shop_address.setBounds(215, 360, 179, 22);
+		shopkeeper_shop_address.setColumns(10);
+		contentPane.add(shopkeeper_shop_address);
 		
 		JButton btnNewButton = new JButton("Sign Up");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent arg0) {
-				dispose();
-				ShopkeeperDashboard sd = new ShopkeeperDashboard();
-				sd.setVisible(true);
+		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton.setBounds(435, 340, 190, 40);
+		btnNewButton.addMouseListener(new MouseAdapter() 
+		{
+			public void mouseClicked(MouseEvent arg0) 
+			{
+				String shopkeeperName = shopkeeper_name.getText();
+                String shopkeeperPhone = shopkeeper_phone.getText();
+                String shopkeeperEmail = shopkeeper_email.getText();
+                String shopkeeperPswd = shopkeeper_password.getText();
+                String shopkeeperConfirmpswd = shopkeeper_confirm_password.getText();
+                String shopkeeperShopName = shopkeeper_shop_name.getText();
+                String shopkeeperShopAddress = shopkeeper_shop_address.getText();
+                int len = shopkeeperPhone.length();
+
+                String msg = "" + shopkeeperName;
+                msg += " \n";
+                if (len != 10) 
+                {
+                    JOptionPane.showMessageDialog(null, "Enter a valid mobile number !");
+                }
+                
+                if (shopkeeperName.length()==0||shopkeeperPhone.length()==0||shopkeeperEmail.length()==0||shopkeeperPswd.length()==0||shopkeeperConfirmpswd.length()==0||shopkeeperShopName.length()==0||shopkeeperShopAddress.length()==0) 
+                {
+                    JOptionPane.showMessageDialog(null, "Please fill out all the fields.");
+                }
+                
+                if (shopkeeperPswd.compareTo(shopkeeperConfirmpswd)!=0) 
+                {
+                    JOptionPane.showMessageDialog(null, "Password did not match with the confirmed password !");
+                }
+
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aamdani", "root", "root");
+
+                    String query = "INSERT INTO shopkeeper_sign_up values('" + shopkeeperName + "','" + shopkeeperPhone + "','" + shopkeeperEmail + "','" +
+                    		shopkeeperPswd + "','" + shopkeeperConfirmpswd + "','" + shopkeeperShopName + "','" + shopkeeperShopAddress + "')";
+
+                    Statement sta = connection.createStatement();
+                    int x = sta.executeUpdate(query);
+                    //if (x == 0) {
+                      //  JOptionPane.showMessageDialog(btnNewButton, "This is alredy exist");
+                    //} else {
+                      //  JOptionPane.showMessageDialog(btnNewButton,
+                        //    "Welcome, " + msg + "Your account is sucessfully created");
+                    
+                    connection.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+				
+				
+				//dispose();
+				//PostJobButtonClicked pjbc = new PostJobButtonClicked();
+				//pjbc.setVisible(true);
 			}
 		});
 		btnNewButton.setBorder(null);
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.setBackground(SystemColor.textHighlight);
 		btnNewButton.setFont(new Font("Calibri", Font.BOLD, 20));
-		btnNewButton.setBounds(448, 337, 187, 40);
 		contentPane.add(btnNewButton);
 		
 		JTextArea txtrCreateAccount = new JTextArea();
+		txtrCreateAccount.setBounds(49, 33, 233, 31);
 		txtrCreateAccount.setEditable(false);
 		txtrCreateAccount.setFont(new Font("Calibri", Font.BOLD, 30));
 		txtrCreateAccount.setBackground(Color.LIGHT_GRAY);
 		txtrCreateAccount.setText("CREATE ACCOUNT");
-		txtrCreateAccount.setBounds(49, 33, 233, 31);
 		contentPane.add(txtrCreateAccount);
 		
 		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(430, 95, 195, 200);
 		Image img = new ImageIcon(this.getClass().getResource("/NewUserIcon.png")).getImage();
 		lblNewLabel.setIcon(new ImageIcon(img));
-		lblNewLabel.setBounds(440, 95, 195, 200);
 		contentPane.add(lblNewLabel);
+		
+		shopkeeper_password = new JPasswordField();
+		shopkeeper_password.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_password.setFont(new Font("Calibri", Font.PLAIN, 14));
+		shopkeeper_password.setBounds(215, 225, 179, 21);
+		contentPane.add(shopkeeper_password);
+		
+		shopkeeper_confirm_password = new JPasswordField();
+		shopkeeper_confirm_password.setFont(new Font("Calibri", Font.PLAIN, 14));
+		shopkeeper_confirm_password.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		shopkeeper_confirm_password.setBounds(215, 270, 179, 21);
+		contentPane.add(shopkeeper_confirm_password);
 	}
 }
