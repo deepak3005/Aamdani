1f1bef09319493eabe38d0303c8346dc634451c0
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-03-03 01:40:26 +0530

    Update NewStudentSignUp.java

--- Source Code/NewStudentSignUp.java
+++ Source Code/NewStudentSignUp.java
@@ -72,103 +72,115 @@ public class NewStudentSignUp extends JFrame {
 	 * Create the frame.
 	 */
 	public NewStudentSignUp() {
+		setTitle("Student Sign Up");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 680, 470);
+		setBounds(100, 100, 1000, 700);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JTextArea txtrName = new JTextArea();
-		txtrName.setBounds(60, 85, 59, 21);
+		txtrName.setForeground(Color.WHITE);
+		txtrName.setBounds(113, 170, 59, 24);
 		txtrName.setEditable(false);
-		txtrName.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrName.setBackground(Color.LIGHT_GRAY);
+		txtrName.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrName.setBackground(Color.DARK_GRAY);
 		txtrName.setText("Name");
 		contentPane.add(txtrName);
 		
 		JTextArea txtrPhoneNo = new JTextArea();
-		txtrPhoneNo.setBounds(60, 120, 86, 22);
+		txtrPhoneNo.setForeground(Color.WHITE);
+		txtrPhoneNo.setBounds(113, 220, 107, 24);
 		txtrPhoneNo.setEditable(false);
-		txtrPhoneNo.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrPhoneNo.setBackground(Color.LIGHT_GRAY);
+		txtrPhoneNo.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrPhoneNo.setBackground(Color.DARK_GRAY);
 		txtrPhoneNo.setText("Phone No.");
 		contentPane.add(txtrPhoneNo);
 		
 		JTextArea txtrEmail = new JTextArea();
-		txtrEmail.setBounds(60, 155, 59, 21);
+		txtrEmail.setForeground(Color.WHITE);
+		txtrEmail.setBounds(113, 270, 59, 24);
 		txtrEmail.setEditable(false);
-		txtrEmail.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrEmail.setBackground(Color.LIGHT_GRAY);
+		txtrEmail.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrEmail.setBackground(Color.DARK_GRAY);
 		txtrEmail.setText("Email");
 		contentPane.add(txtrEmail);
 		
 		JTextArea txtrSetPassword = new JTextArea();
-		txtrSetPassword.setBounds(60, 190, 96, 22);
+		txtrSetPassword.setForeground(Color.WHITE);
+		txtrSetPassword.setBounds(113, 320, 133, 24);
 		txtrSetPassword.setEditable(false);
-		txtrSetPassword.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrSetPassword.setBackground(Color.LIGHT_GRAY);
+		txtrSetPassword.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrSetPassword.setBackground(Color.DARK_GRAY);
 		txtrSetPassword.setText("Set Password");
 		contentPane.add(txtrSetPassword);
 		
 		JTextArea txtrShopName = new JTextArea();
-		txtrShopName.setBounds(60, 365, 86, 22);
+		txtrShopName.setForeground(Color.WHITE);
+		txtrShopName.setBounds(113, 570, 86, 24);
 		txtrShopName.setEditable(false);
-		txtrShopName.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrShopName.setBackground(Color.LIGHT_GRAY);
+		txtrShopName.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrShopName.setBackground(Color.DARK_GRAY);
 		txtrShopName.setText("SAP ID");
 		contentPane.add(txtrShopName);
 		
 		JTextArea txtrAddress = new JTextArea();
-		txtrAddress.setBounds(60, 260, 59, 22);
+		txtrAddress.setForeground(Color.WHITE);
+		txtrAddress.setBounds(113, 420, 86, 24);
 		txtrAddress.setEditable(false);
-		txtrAddress.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrAddress.setBackground(Color.LIGHT_GRAY);
+		txtrAddress.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrAddress.setBackground(Color.DARK_GRAY);
 		txtrAddress.setText("Course");
 		contentPane.add(txtrAddress);
 		
 		JTextArea txtrDistanceFromUpes = new JTextArea();
-		txtrDistanceFromUpes.setBounds(60, 225, 133, 22);
+		txtrDistanceFromUpes.setForeground(Color.WHITE);
+		txtrDistanceFromUpes.setBounds(113, 370, 173, 24);
 		txtrDistanceFromUpes.setEditable(false);
-		txtrDistanceFromUpes.setFont(new Font("Calibri", Font.BOLD, 16));
-		txtrDistanceFromUpes.setBackground(Color.LIGHT_GRAY);
+		txtrDistanceFromUpes.setFont(new Font("Calibri", Font.BOLD, 22));
+		txtrDistanceFromUpes.setBackground(Color.DARK_GRAY);
 		txtrDistanceFromUpes.setText("Confirm Password");
 		contentPane.add(txtrDistanceFromUpes);
 		
 		student_name = new JTextField();
+		student_name.setForeground(Color.DARK_GRAY);
 		student_name.setAutoscrolls(false);
 		student_name.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_name.setBounds(235, 80, 184, 21);
-		student_name.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_name.setBounds(341, 165, 211, 28);
+		student_name.setFont(new Font("Calibri", Font.BOLD, 18));
 		contentPane.add(student_name);
 		student_name.setColumns(10);
 		
 		student_phone = new JTextField();
-		student_phone.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_phone.setForeground(Color.DARK_GRAY);
+		student_phone.setFont(new Font("Calibri", Font.BOLD, 18));
 		student_phone.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_phone.setBounds(235, 115, 184, 22);
+		student_phone.setBounds(341, 215, 211, 28);
 		student_phone.setColumns(10);
 		contentPane.add(student_phone);
 		
 		student_email = new JTextField();
-		student_email.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_email.setForeground(Color.DARK_GRAY);
+		student_email.setFont(new Font("Calibri", Font.BOLD, 18));
 		student_email.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_email.setBounds(235, 150, 184, 21);
+		student_email.setBounds(341, 265, 211, 28);
 		student_email.setColumns(10);
 		contentPane.add(student_email);
 		
 		student_specialization = new JTextField();
-		student_specialization.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_specialization.setForeground(Color.DARK_GRAY);
+		student_specialization.setFont(new Font("Calibri", Font.BOLD, 18));
 		student_specialization.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_specialization.setBounds(235, 290, 184, 22);
+		student_specialization.setBounds(341, 465, 211, 28);
 		student_specialization.setColumns(10);
 		contentPane.add(student_specialization);
 	  
 		JButton btnNewButton = new JButton("Sign Up");
 		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton.setBounds(475, 255, 126, 40);
+		btnNewButton.setBounds(645, 485, 232, 50);
 		btnNewButton.addMouseListener(new MouseAdapter() 
 		{
 			public void mouseClicked(MouseEvent arg0) 
@@ -248,79 +260,105 @@ public class NewStudentSignUp extends JFrame {
 		btnNewButton.setBorder(null);
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.setBackground(SystemColor.textHighlight);
-		btnNewButton.setFont(new Font("Calibri", Font.BOLD, 20));
+		btnNewButton.setFont(new Font("Calibri", Font.BOLD, 26));
 		contentPane.add(btnNewButton);
 		
 		JTextArea txtrCreateAccount = new JTextArea();
-		txtrCreateAccount.setBounds(57, 25, 233, 31);
+		txtrCreateAccount.setForeground(Color.WHITE);
+		txtrCreateAccount.setBounds(105, 90, 307, 40);
 		txtrCreateAccount.setEditable(false);
-		txtrCreateAccount.setFont(new Font("Calibri", Font.BOLD, 30));
-		txtrCreateAccount.setBackground(Color.LIGHT_GRAY);
+		txtrCreateAccount.setFont(new Font("Calibri", Font.BOLD, 40));
+		txtrCreateAccount.setBackground(Color.DARK_GRAY);
 		txtrCreateAccount.setText("CREATE ACCOUNT");
 		contentPane.add(txtrCreateAccount);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(475, 119, 133, 128);
-		Image img = new ImageIcon(this.getClass().getResource("/New-Student-Medium.png")).getImage();
+		lblNewLabel.setBounds(631, 185, 256, 249);
+		Image img = new ImageIcon(this.getClass().getResource("/New-Student-Large.png")).getImage();
 		lblNewLabel.setIcon(new ImageIcon(img));
 		contentPane.add(lblNewLabel);
 		
 		student_password = new JPasswordField();
+		student_password.setForeground(Color.DARK_GRAY);
 		student_password.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_password.setFont(new Font("Calibri", Font.PLAIN, 14));
-		student_password.setBounds(235, 185, 184, 21);
+		student_password.setFont(new Font("Calibri", Font.BOLD, 18));
+		student_password.setBounds(341, 315, 211, 28);
 		contentPane.add(student_password);
 		
 		student_confirm_password = new JPasswordField();
-		student_confirm_password.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_confirm_password.setForeground(Color.DARK_GRAY);
+		student_confirm_password.setFont(new Font("Calibri", Font.BOLD, 18));
 		student_confirm_password.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_confirm_password.setBounds(235, 220, 184, 21);
+		student_confirm_password.setBounds(341, 365, 211, 28);
 		contentPane.add(student_confirm_password);
 		
 		student_degree = new JComboBox();
+		student_degree.setForeground(Color.DARK_GRAY);
 		student_degree.setName("");
 		student_degree.setMaximumRowCount(10);
-		student_degree.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_degree.setFont(new Font("Calibri", Font.BOLD, 16));
 		student_degree.setModel(new DefaultComboBoxModel(new String[] {"B.Tech", "M.Tech", "BBA", "MBA", "BCA", "MCA", "B.SC", "M.SC", "B.DES", "M.DES", "LL.B", "LLM", "BA", "MA", "B.COM", "B.Pharma", "BJMC"}));
-		student_degree.setBounds(235, 255, 67, 21);
+		student_degree.setBounds(341, 415, 76, 28);
 		contentPane.add(student_degree);
 		
 		JTextArea txtrYearOfStudy = new JTextArea();
+		txtrYearOfStudy.setForeground(Color.WHITE);
 		txtrYearOfStudy.setText("Year of study");
-		txtrYearOfStudy.setFont(new Font("Calibri", Font.BOLD, 16));
+		txtrYearOfStudy.setFont(new Font("Calibri", Font.BOLD, 22));
 		txtrYearOfStudy.setEditable(false);
-		txtrYearOfStudy.setBackground(Color.LIGHT_GRAY);
-		txtrYearOfStudy.setBounds(60, 330, 96, 22);
+		txtrYearOfStudy.setBackground(Color.DARK_GRAY);
+		txtrYearOfStudy.setBounds(113, 520, 133, 24);
 		contentPane.add(txtrYearOfStudy);
 		
 		student_year = new JTextField();
-		student_year.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_year.setForeground(Color.DARK_GRAY);
+		student_year.setFont(new Font("Calibri", Font.BOLD, 18));
 		student_year.setColumns(10);
 		student_year.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_year.setBounds(235, 325, 184, 22);
+		student_year.setBounds(341, 515, 211, 28);
 		contentPane.add(student_year);
 		
 		student_course = new JComboBox();
+		student_course.setForeground(Color.DARK_GRAY);
 		student_course.setModel(new DefaultComboBoxModel(new String[] {"CSE", "Chemical", "Design", "Upstream"}));
 		student_course.setName("");
 		student_course.setMaximumRowCount(10);
-		student_course.setFont(new Font("Calibri", Font.PLAIN, 14));
-		student_course.setBounds(312, 255, 107, 21);
+		student_course.setFont(new Font("Calibri", Font.BOLD, 16));
+		student_course.setBounds(427, 415, 125, 28);
 		contentPane.add(student_course);
 		
 		JTextArea txtrSpecialization = new JTextArea();
+		txtrSpecialization.setForeground(Color.WHITE);
 		txtrSpecialization.setText("Specialization (if any)");
-		txtrSpecialization.setFont(new Font("Calibri", Font.BOLD, 16));
+		txtrSpecialization.setFont(new Font("Calibri", Font.BOLD, 22));
 		txtrSpecialization.setEditable(false);
-		txtrSpecialization.setBackground(Color.LIGHT_GRAY);
-		txtrSpecialization.setBounds(60, 295, 151, 22);
+		txtrSpecialization.setBackground(Color.DARK_GRAY);
+		txtrSpecialization.setBounds(113, 470, 199, 24);
 		contentPane.add(txtrSpecialization);
 		
 		student_sapid = new JTextField();
-		student_sapid.setFont(new Font("Calibri", Font.PLAIN, 14));
+		student_sapid.setForeground(Color.DARK_GRAY);
+		student_sapid.setFont(new Font("Calibri", Font.BOLD, 18));
 		student_sapid.setColumns(10);
 		student_sapid.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		student_sapid.setBounds(235, 360, 184, 22);
+		student_sapid.setBounds(341, 565, 211, 28);
 		contentPane.add(student_sapid);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setToolTipText("Back");
+		lblNewLabel_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblNewLabel_1.addMouseListener(new MouseAdapter() 
+		{
+			public void mouseClicked(MouseEvent e) 
+			{
+				dispose();
+				FindJobButtonClicked fjbc2 = new FindJobButtonClicked();
+				fjbc2.setVisible(true);
+			}
+		});
+		Image img1 = new ImageIcon(this.getClass().getResource("/back-arrow.png")).getImage();
+		lblNewLabel_1.setIcon(new ImageIcon(img1));
+		lblNewLabel_1.setBounds(15, 15, 59, 50);
+		contentPane.add(lblNewLabel_1);
 	}
 }
