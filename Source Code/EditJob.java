d36841d3d79364b2c1d9ce1763911f49675bd553
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-03-12 22:32:02 +0530

    Update EditJob.java

--- Source Code/EditJob.java
+++ Source Code/EditJob.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.SystemColor;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -33,6 +34,7 @@ import net.proteanit.sql.DbUtils;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
 
 public class EditJob extends JFrame {
 
@@ -76,32 +78,31 @@ public class EditJob extends JFrame {
 	public void setValuesToEditPage()
 	{
 		String getEmailId = PostJobButtonClicked.shopkeeper_email_entered.getText();
-		//jobnoentered = ShopkeeperDashboard.Job_Number_Entered.getText();
 		
-		try 
-        {
-        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aamdani", "root", "root");
-               
-            String query = "SELECT Title,Decription,Location,Distance_From_UPES,Hourly_Stipend,Start_Time,End_Time FROM jobs_posted WHERE Email_Address='"+getEmailId+"'  and Title ='"+ShopkeeperDashboard.SelectedRowTitle+"' and Location ='"+ShopkeeperDashboard.SelectedRowLocation+"' and Distance_From_UPES ='"+ShopkeeperDashboard.SelectedRowDistance+"' and Hourly_Stipend ='"+ShopkeeperDashboard.SelectedRowStipend+"' and Start_Time ='"+ShopkeeperDashboard.SelectedRowStart+"' and End_Time ='"+ShopkeeperDashboard.SelectedRowEnd+"'";
+			try 
+	        {
+	        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aamdani", "root", "root");
+	               
+	            String query = "SELECT Title,Decription,Location,Distance_From_UPES_In_KMs,Hourly_Stipend_INR,Start_Time,End_Time FROM jobs_posted WHERE Email_Address='"+getEmailId+"'  and Title ='"+ShopkeeperDashboard.SelectedRowTitle+"' and Location ='"+ShopkeeperDashboard.SelectedRowLocation+"' and Distance_From_UPES_In_KMs ='"+ShopkeeperDashboard.SelectedRowDistance+"' and Hourly_Stipend_INR ='"+ShopkeeperDashboard.SelectedRowStipend+"' and Start_Time ='"+ShopkeeperDashboard.SelectedRowStart+"' and End_Time ='"+ShopkeeperDashboard.SelectedRowEnd+"'";
 
-            Statement sta = connection.createStatement();
-            ResultSet rs = sta.executeQuery(query);
-            rs.next();
-            
-            Job_Title_Entered.setText(rs.getString(1));
-            Job_Description_Entered.setText(rs.getString(2));
-            Job_Location_Entered.setText(rs.getString(3));
-            Job_Distance_Entered.setText(rs.getString(4));
-            Job_Stipend_Entered.setText(rs.getString(5));
-            Job_Timing_From_Entered.setSelectedItem(rs.getString(6));
-            Job_Timing_To_Entered.setSelectedItem(rs.getString(7));
-            
-            connection.close();
-        }
-        catch (Exception exception) 
-        {
-            exception.printStackTrace();
-        }
+	            Statement sta = connection.createStatement();
+	            ResultSet rs = sta.executeQuery(query);
+	            rs.next();
+	            
+	            Job_Title_Entered.setText(rs.getString(1));
+	            Job_Description_Entered.setText(rs.getString(2));
+	            Job_Location_Entered.setText(rs.getString(3));
+	            Job_Distance_Entered.setText(rs.getString(4));
+	            Job_Stipend_Entered.setText(rs.getString(5));
+	            Job_Timing_From_Entered.setSelectedItem(rs.getString(6));
+	            Job_Timing_To_Entered.setSelectedItem(rs.getString(7));
+	            
+	            connection.close();
+	        }
+	        catch (Exception exception) 
+	        {
+	            exception.printStackTrace();
+	        }
 	}
 	
 
@@ -119,7 +120,9 @@ public class EditJob extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+		Toolkit toolkit = getToolkit();
+		Dimension size = toolkit.getScreenSize();
+		setLocation(size.width/2 - getWidth()/2 , size.height/2 - getHeight()/2);
 		
 		txtJobTitle = new JTextField();
 		txtJobTitle.setBorder(null);
@@ -228,7 +231,7 @@ public class EditJob extends JFrame {
                 
                 if (JobTitle.length()==0||JobDescription.length()==0||JobLocation.length()==0||JobStipend.length()==0||JobDistance.length()==0) 
                 {
-                    JOptionPane.showMessageDialog(null, "Please fill out all the fields.");
+                    JOptionPane.showMessageDialog(null, new JLabel("Please fill out all the fields.", JLabel.CENTER), "Error", JOptionPane.ERROR_MESSAGE);
                 }
 
                 else
@@ -241,8 +244,8 @@ public class EditJob extends JFrame {
                         
                         String query2 = "UPDATE jobs_posted SET Decription='"+JobDescription+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
                         String query3 = "UPDATE jobs_posted SET Location='"+JobLocation+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
-                        String query4 = "UPDATE jobs_posted SET Hourly_Stipend='"+JobStipend+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
-                        String query5 = "UPDATE jobs_posted SET Distance_From_UPES='"+JobDistance+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
+                        String query4 = "UPDATE jobs_posted SET Hourly_Stipend_INR='"+JobStipend+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
+                        String query5 = "UPDATE jobs_posted SET Distance_From_UPES_In_KMs='"+JobDistance+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
                         String query6 = "UPDATE jobs_posted SET Start_Time='"+JobTimingsFrom+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
                         String query7 = "UPDATE jobs_posted SET End_Time='"+JobTimingsTo+"' WHERE Email_Address='"+ShopkeeperEmail+"' and Title='"+JobTitle+"'";
                         
@@ -338,14 +341,15 @@ public class EditJob extends JFrame {
 		contentPane.add(Job_Distance_Entered);
 		
 		Job_Timing_From_Entered = new JComboBox();
-		Job_Timing_From_Entered.setModel(new DefaultComboBoxModel(new String[] {"00 : 00", "02 : 00", "03 : 00", "04 : 00", "05 : 00", "06 : 00", "07 : 00", "08 : 00", "09 : 00", "10 : 00", "11 : 00", "12 : 00", "13 : 00", "14 : 00", "15 : 00", "16 : 00", "17 : 00", "18 : 00", "19 : 00", "20 : 00", "21 : 00", "22 : 00", "23 : 00", "24 : 00"}));
+		Job_Timing_From_Entered.setModel(new DefaultComboBoxModel(new String[] {"00 : 00", "01 : 00", "02 : 00", "03 : 00", "04 : 00", "05 : 00", "06 : 00", "07 : 00", "08 : 00", "09 : 00", "10 : 00", "11 : 00", "12 : 00", "13 : 00", "14 : 00", "15 : 00", "16 : 00", "17 : 00", "18 : 00", "19 : 00", "20 : 00", "21 : 00", "22 : 00", "23 : 00", "24 : 00"}));
 		Job_Timing_From_Entered.setMaximumRowCount(24);
 		Job_Timing_From_Entered.setFont(new Font("Cambria", Font.PLAIN, 15));
 		Job_Timing_From_Entered.setBounds(655, 411, 73, 28);
 		contentPane.add(Job_Timing_From_Entered);
 		
 		Job_Timing_To_Entered = new JComboBox();
-		Job_Timing_To_Entered.setModel(new DefaultComboBoxModel(new String[] {"00 : 00", "02 : 00", "03 : 00", "04 : 00", "05 : 00", "06 : 00", "07 : 00", "08 : 00", "09 : 00", "10 : 00", "11 : 00", "12 : 00", "13 : 00", "14 : 00", "15 : 00", "16 : 00", "17 : 00", "18 : 00", "19 : 00", "20 : 00", "21 : 00", "22 : 00", "23 : 00", "24 : 00"}));
+		Job_Timing_To_Entered.setModel(new DefaultComboBoxModel(new String[] {"00 : 00", "01 : 00", "02 : 00", "03 : 00", "04 : 00", "05 : 00", "06 : 00", "07 : 00", "08 : 00", "09 : 00", "10 : 00", "11 : 00", "12 : 00", "13 : 00", "14 : 00", "15 : 00", "16 : 00", "17 : 00", "18 : 00", "19 : 00", "20 : 00", "21 : 00", "22 : 00", "23 : 00", "24 : 00"}));
+		Job_Timing_To_Entered.setSelectedIndex(24);
 		Job_Timing_To_Entered.setMaximumRowCount(24);
 		Job_Timing_To_Entered.setFont(new Font("Cambria", Font.PLAIN, 15));
 		Job_Timing_To_Entered.setBounds(834, 411, 73, 28);
