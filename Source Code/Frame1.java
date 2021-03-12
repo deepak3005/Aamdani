0acbeea490ca493a5f498f9b4999c21fe3c62840
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-03-12 22:30:14 +0530

    Update Frame1.java

--- Source Code/Frame1.java
+++ Source Code/Frame1.java
@@ -60,7 +60,7 @@ public class Frame1 {
 		txtrWelcome.setEditable(false);
 		txtrWelcome.setFont(new Font("Cambria", Font.BOLD, 60));
 		txtrWelcome.setText("Welcome To AAMDANI");
-		txtrWelcome.setBounds(185, 74, 630, 69);
+		txtrWelcome.setBounds(185, 60, 630, 69);
 		frmAamdani.getContentPane().add(txtrWelcome);
 		
 		JLabel lblNewLabel = new JLabel("");
@@ -72,7 +72,7 @@ public class Frame1 {
 				sf.setVisible(true);
 			}
 		});
-		lblNewLabel.setBounds(375, 190, 250, 250);
+		lblNewLabel.setBounds(375, 160, 250, 250);
 		Image img = new ImageIcon(this.getClass().getResource("/handshake.png")).getImage();
 		lblNewLabel.setIcon(new ImageIcon(img));
 		frmAamdani.getContentPane().add(lblNewLabel);
@@ -80,19 +80,37 @@ public class Frame1 {
 		JTextArea txtrBlahBlahBlah = new JTextArea();
 		txtrBlahBlahBlah.setForeground(Color.WHITE);
 		txtrBlahBlahBlah.setEditable(false);
-		txtrBlahBlahBlah.setText("Description sort of ...");
-		txtrBlahBlahBlah.setFont(new Font("Cambria", Font.BOLD, 20));
+		txtrBlahBlahBlah.setText("A platform for college students to find non-technical part-time jobs near their colleges in order to earn their basic expenses and also making it easier for employers as well to find candidates willing to work on an hourly stipend basis.");
+		txtrBlahBlahBlah.setFont(new Font("Cambria", Font.BOLD, 18));
 		txtrBlahBlahBlah.setBackground(Color.DARK_GRAY);
-		txtrBlahBlahBlah.setBounds(404, 487, 192, 28);
+		txtrBlahBlahBlah.setBounds(173, 440, 651, 28);
 		frmAamdani.getContentPane().add(txtrBlahBlahBlah);
 		
 		JTextArea txtrClickOnThe = new JTextArea();
 		txtrClickOnThe.setForeground(Color.WHITE);
 		txtrClickOnThe.setEditable(false);
 		txtrClickOnThe.setText("Click on the logo above to Get Started");
-		txtrClickOnThe.setFont(new Font("Cambria", Font.BOLD, 20));
+		txtrClickOnThe.setFont(new Font("Cambria", Font.BOLD, 22));
 		txtrClickOnThe.setBackground(Color.DARK_GRAY);
-		txtrClickOnThe.setBounds(330, 534, 340, 28);
+		txtrClickOnThe.setBounds(308, 560, 384, 28);
 		frmAamdani.getContentPane().add(txtrClickOnThe);
+		
+		JTextArea txtrCollegesInOrder = new JTextArea();
+		txtrCollegesInOrder.setText("colleges in order to earn their basic expenses and also making it easier for employers as well to find candidates willing to work on an hourly stipend basis.");
+		txtrCollegesInOrder.setForeground(Color.WHITE);
+		txtrCollegesInOrder.setFont(new Font("Cambria", Font.BOLD, 18));
+		txtrCollegesInOrder.setEditable(false);
+		txtrCollegesInOrder.setBackground(Color.DARK_GRAY);
+		txtrCollegesInOrder.setBounds(141, 470, 716, 28);
+		frmAamdani.getContentPane().add(txtrCollegesInOrder);
+		
+		JTextArea txtrBlahBlahBlah_2 = new JTextArea();
+		txtrBlahBlahBlah_2.setText("as well to find candidates willing to work on an hourly stipend basis.");
+		txtrBlahBlahBlah_2.setForeground(Color.WHITE);
+		txtrBlahBlahBlah_2.setFont(new Font("Cambria", Font.BOLD, 20));
+		txtrBlahBlahBlah_2.setEditable(false);
+		txtrBlahBlahBlah_2.setBackground(Color.DARK_GRAY);
+		txtrBlahBlahBlah_2.setBounds(188, 500, 623, 28);
+		frmAamdani.getContentPane().add(txtrBlahBlahBlah_2);
 	}
 }
